ba7d9f897ed0f63d72f0801b16d60dd899f41a9b
Daniel <daniel-2193@hotmail.com>
2023-12-05 21:07:56 -0300

    configurando o Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-function testeGulp(cb){
-    console.log('olá')
-    cb();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle:'compressed'}))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = testeGulp;
+exports.default = styles;
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
