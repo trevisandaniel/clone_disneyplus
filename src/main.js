83cf33b76253c96afd0572b236e2092b8b893f76
Daniel <daniel-2193@hotmail.com>
2024-01-16 21:25:03 -0300

    arrumando a sessao faq

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
 
     for (let i=0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao) {
@@ -12,7 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    //faq
+    for (let i = 0; 1 < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    
+     const elementoPai = elemento.target.parentNode;
+
+     elementoPai.classList.toggle(classe)
+}
 
 function removeActiveButton(){
     const buttons = document.querySelectorAll('[data-tab-button]');
